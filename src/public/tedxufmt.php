5eb644d89c5d837ae2b9b651c7b30fa4bfe1fe80
Alvaro Viebrantz <alvarowolfx@gmail.com>
2018-03-01 10:49:36 -0400

    [feat] Alterações de texto principal

--- src/public/tedxufmt.php
+++ src/public/tedxufmt.php
@@ -6,13 +6,13 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2 to-animate">
                         <h3 style="text-align:center;font-size: 20px;"> 
-                            Estamos diante de milhões de informações a todo momento e a palma da mão, porém nosso cérebro processa, interpreta e mantém a atenção no que considera importante, sendo facilmente desviado quando algo não o instiga mais. 
+                            Estamos diante de milhões de informações a todo momento e a palma da mão, porém nosso cérebro processa, interpreta e mantém a atenção no que considera importante, sendo facilmente desviado quando algo não o instiga mais.                             
                         </h3>
                         <h3 style="text-align:center;font-size: 20px;"> 
-                            Diante disto, o que você ainda não viu que te traria brilho no olhar? Que música você ainda não ouviu que te faria sentir o coração vibrar? O que você ainda não provou que te daria forças para encarar? Que histórias ainda não conheceu que te relembraria o passado, te fixaria no presente e daria esperanças no futuro? Estamos sempre em busca de uma boa conversa e novas descobertas, capaz de manter a nossa concentração e nos levar a inúmeras sensações, desde aumentar nosso senso crítico a nos inspirar. 
+                            Diante disto, o que você ainda não viu que te traria brilho no olhar? Que música você ainda não ouviu que te faria sentir o coração vibrar? O que você ainda não provou que te daria forças para encarar? Que histórias ainda não conheceu que te relembraria o passado, te fixaria no presente e daria esperanças no futuro? Estamos sempre em busca de uma boa conversa e novas descobertas, capaz de manter a nossa concentração e aguçar nossa curiosidade, nos levando a inúmeras sensações, desde aumentar nosso senso crítico a nos inspirar.                             
                         </h3>    
-                        <h3 style="text-align:center;font-size: 20px;"> 
-                            O <b>TEDxUFMT</b>  quer mostrar toda a diversidade e potencial do nosso estado, sendo instrumento de catálises, fundamentais para construir o futuro que queremos. Cidades inteligentes, um país com oportunidade para todos, valorização cultural, inovação e o papel da universidade na formação de profissionais para as novas perspectivas de mercado, serão algumas das áreas discutidas.                             
+                        <h3 style="text-align:center;font-size: 20px;">                             
+                            O <b>TEDxUFMT</b> quer mostrar toda a diversidade e potencial do nosso estado, sendo instrumento de catálises, fundamentais para acelerar transformações e construir o futuro que queremos. Cidades inteligentes, um país com oportunidade para todos, valorização cultural, inovação e o papel da universidade na formação de profissionais para as novas perspectivas de mercado, serão algumas das áreas discutidas. 
                         </h3>
 
                         <h3 style="text-align:center; color:#000; font-size: 20px; "> 
